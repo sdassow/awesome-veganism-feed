0803112a7ee659b4d2de6da26af9760c5ac4103d
Simon Dassow <simon@fabams.nl>
2023-09-13 13:33:13 +0200

    Write results to files, make directories configurable, and clean up

--- main.go
+++ main.go
@@ -1,47 +1,71 @@
 package main
 
 import (
-	"log"
+	"bytes"
+	"flag"
 	"fmt"
-	"time"
+	"log"
+	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/gorilla/feeds"
+	"github.com/natefinch/atomic"
 )
 
 func main() {
-	path := "/home/simon/Code/awesome-veganism"
+	var destdir string
+	var workdir string
+	var verbose bool
 
-	r, err := git.PlainOpen(path)
+	flag.StringVar(&destdir, "destdir", ".", "destination directory for feed files")
+	flag.StringVar(&workdir, "workdir", ".", "working directory with a git repository")
+	flag.BoolVar(&verbose, "verbose", false, "turn on verbose mode")
+	flag.Parse()
+
+	// regular expression to find relevant items in diffs
+	re, err := regexp.Compile(`\n([+-])\s*[-] \[([^\]]+)\]\(([^\)]+)\) [-] ([^\n]+)`)
 	if err != nil {
-		log.Fatalf("failed to open repository: %s: %v", path, err)
+		log.Fatalf("failed to compile regular expression: %v", err)
 	}
 
-	// ... retrieving the HEAD reference
+	// open checked out repository
+	r, err := git.PlainOpen(workdir)
+	if err != nil {
+		log.Fatalf("failed to open repository: %s: %v", workdir, err)
+	}
+
+	// file to work with
+	workfile := "README.md"
+
+	// make sure file exists
+	if _, err := os.Stat(filepath.Join(workdir, workfile)); err != nil {
+		log.Fatalf("failed to locate file: %v", err)
+	}
+
+	// get HEAD reference
 	ref, err := r.Head()
 	if err != nil {
 		log.Fatalf("failed to get HEAD reference: %v", err)
 	}
 
-	logfile := "README.md"
 	logopts := &git.LogOptions{
 		From:     ref.Hash(),
-		FileName: &logfile,
+		FileName: &workfile,
 		Order:    git.LogOrderCommitterTime,
-		//Order: git.LogOrderDFSPost,
 	}
 
-	// ... retrieves the commit history
+	// get commit history
 	iter, err := r.Log(logopts)
 	if err != nil {
 		log.Fatalf("failed to get log: %v", err)
 	}
 
+	// build list with all commits
 	var commits []*object.Commit
-
-	// ... just iterates over the commits
 	err = iter.ForEach(func(c *object.Commit) error {
 		commits = append(commits, c)
 
@@ -51,18 +75,16 @@ func main() {
 		log.Fatalf("failed to iterate commit log: %v", err)
 	}
 
-	re, err := regexp.Compile(`\n([+-])\s*[-] \[([^\]]+)\]\(([^\)]+)\) [-] ([^\n]+)`)
-	if err != nil {
-		log.Fatalf("failed to compile regular expression: %v", err)
+	if len(commits) == 0 {
+		log.Fatal("failed to find commits")
 	}
 
-	now := time.Now()
+	// setup feed
 	feed := &feeds.Feed{
 		Title:       "AwesomeVeganism Feed",
 		Link:        &feeds.Link{Href: "https://awesome-veganism.com/"},
 		Description: "A curated list of awesome resources, pointers, and tips to make veganism easy and accessible to everyone.",
-		Author:      &feeds.Author{Name: "Yeah, hmm", Email: "jmoiron@jmoiron.net"},
-		Created:     now,
+		Created: commits[len(commits)-1].Author.When,
 	}
 
 	for n := len(commits) - 1; n >= 0; n-- {
@@ -70,25 +92,21 @@ func main() {
 
 		// skip initial commit in this project as it happens to have no relevant content
 		if n == 0 {
-			// use date of first commit for date of creation
-			feed.Created = c.Author.When
 			break
 		}
 
 		p := commits[n-1]
 
-		log.Printf("===>> commit: %s by %s at %s: %s", p.Hash, p.Author.Name, p.Author.When, p.Message)
+		if verbose {
+			log.Printf("===> commit: %s by %s at %s: %s", p.Hash, p.Author.Name, p.Author.When, p.Message)
+		}
 
 		patch, err := c.Patch(p)
 		if err != nil {
-			log.Printf("failed to get patch: %v", err)
+			log.Fatalf("failed to get patch: %v", err)
 		}
 
-		//log.Printf("patch: %s", patch)
-
 		matches := re.FindAllStringSubmatch(patch.String(), -1)
-
-		//log.Printf("matches: %v", matches)
 
 		// filter out moving items around: a plus and a minus cancel each other out
 		changes := make(map[string]int)
@@ -108,9 +126,9 @@ func main() {
 			changes[m[2]] = v
 		}
 
-		log.Printf("changes: %v", changes)
-
-		continue
+		if verbose {
+			log.Printf("changes: %v", changes)
+		}
 
 		for _, m := range matches {
 			// skip when there was only a move of an entry
@@ -124,24 +142,58 @@ func main() {
 				t = "Removal"
 			}
 
-			log.Printf("====>> %s: %s -- %s -- %s", t, m[2], m[3], m[4])
+			if verbose {
+				log.Printf("=====>> %s: %s -- %s -- %s", t, m[2], m[3], m[4])
+			}
 
 			feed.Items = append(feed.Items, &feeds.Item{
 				Title:       fmt.Sprintf("%s of %s", t, m[2]),
 				Link:        &feeds.Link{Href: m[3]},
 				Description: m[4],
-				Author:      &feeds.Author{Name: p.Author.Name, Email: "jmoiron@jmoiron.net"},
+				Author:      &feeds.Author{Name: p.Author.Name},
 				Created:     p.Author.When,
 			})
 
 			feed.Updated = p.Author.When
 		}
-
 	}
 
-	if feed == nil {
+	atom, err := feed.ToAtom()
+	if err != nil {
+		log.Fatalf("failed to generate atom feed: %v", err)
+	}
+	if err := atomic.WriteFile(filepath.Join(destdir, "feed.xml"), bytes.NewReader([]byte(atom))); err != nil {
+		log.Fatalf("failed to write atom feed: %v", err)
 	}
 
-	fmt.Println(feed.ToRss())
+	json, err := feed.ToJSON()
+	if err != nil {
+		log.Fatalf("failed to generate json feed: %v", err)
+	}
+	if err := atomic.WriteFile(filepath.Join(destdir, "feed.json"), bytes.NewReader([]byte(json))); err != nil {
+		log.Fatalf("failed to write json feed: %v", err)
+	}
 
+	rss, err := feed.ToRss()
+	if err != nil {
+		log.Fatalf("failed to generate rss feed: %v", err)
+	}
+	if err := atomic.WriteFile(filepath.Join(destdir, "feed.rss"), bytes.NewReader([]byte(rss))); err != nil {
+		log.Fatalf("failed to write rss feed: %v", err)
+	}
+
+	files := []string{
+		filepath.Join(destdir, "feed.xml"),
+		filepath.Join(destdir, "feed.json"),
+		filepath.Join(destdir, "feed.rss"),
+	}
+	for _, f := range files {
+		if err := os.Chmod(f, 0644); err != nil {
+			log.Fatalf("failed to change file permission: %s: %v", f, err)
+		}
+	}
+
+	if verbose {
+		log.Printf("files written: %s", strings.Join(files, ", "))
+	}
 }
